22cf22206359c9f0413ecf23118192d4688ceb8b
Aakash Raj <aakashraj416@gmail.com>
2024-04-05 19:59:05 +0530

    key

--- src/Context.jsx
+++ src/Context.jsx
@@ -7,7 +7,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 export const Context = createContext();
 
 export const ContextProvider = ({ children }) => {
-    const genAI = new GoogleGenerativeAI("AIzaSyB7ts87Jw3mK1lKI1ADiNKkDnykvhYyhIY");
+    const genAI = new GoogleGenerativeAI(import.meta.env.VITE_APP_API_KEY);
     const getAnsweredText = async (question) => {
         const model = genAI.getGenerativeModel({ model: "gemini-pro" });
         const result = await model.generateContent(question);
